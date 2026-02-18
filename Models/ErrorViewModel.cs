3493791d9a63d261aaffe414b90c2062badce98e
AgustinQuinteros2030 <agustinquinteros84@gmail.com>
2026-02-18 15:10:21 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoneTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
