7a2ea9cdc4c0849d44bd8762c232854c4103bf81
AgustinQuinteros2030 <agustinquinteros84@gmail.com>
2026-02-20 18:26:49 -0300

    modificacion de la clase usuario y creacion de la clase rol para roles

---
+++ Models/Rol.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoneTask.Models
+{
+    public class Rol : IdentityRole<Guid>
+    {
+        public Rol() : base()
+        {
+
+        }
+
+        public Rol(string name) : base(name)
+        {
+
+        }
+        public Guid Id { get; set; }
+        [Display(Name = "rol")]
+        public override string Name
+        {
+            get => base.Name;
+            set => base.Name = value;
+        }
+
+        public override string NormalizedName { get => base.NormalizedName; set => base.NormalizedName = value; }
+    }
+}
+
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,18 +1,42 @@
 ﻿using DoneTask.Models.DoneTask.Models;
+using DoneTask.Models.Helper;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DoneTask.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser<Guid>
     {
-        public Guid Id { get; set; }
-        public string Username { get; set; }
+        //public Guid Id { get; set; }
+        [Required(ErrorMessage = ErrorMsg.CampoRequerido)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = ErrorMsg.RangoCaracteres)]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = ErrorMsg.SoloLetrasNumeros)]
+        public override string UserName
+        {
+            get => base.UserName;
+            set => base.UserName = value;
+        }
+        [Required(ErrorMessage = ErrorMsg.CampoRequerido)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = ErrorMsg.RangoCaracteres)]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = ErrorMsg.SoloLetras)]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.CampoRequerido)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = ErrorMsg.RangoCaracteres)]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = ErrorMsg.SoloLetras)]
         public string Apellido { get; set; }
         public DateTime FechaAlta { get; set; } = DateTime.Now;
 
-        public string Email { get; set; }
+        [Required(ErrorMessage = ErrorMsg.CampoRequerido)]
+        [EmailAddress(ErrorMessage = ErrorMsg.FormatoEmail)]
+        public override string Email
+        {
+            get => base.Email;
+            set => base.Email = value;
+        }
+      
 
         public List<Tablero> Tableros { get; set; } = new List<Tablero>();
     }
